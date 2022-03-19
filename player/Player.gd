deadad6c69bb895f58688b5bfd7f221343b4a2bd
Niclas Fölster <niclas_folster@hotmail.se>
2022-03-19 09:59:10 +0100

    added animations the right way, and can easily switch to other spritesgit add .

--- player/Player.gd
+++ player/Player.gd
@@ -4,59 +4,47 @@ export var ACCELERATION = 1000
 export var MAX_SPEED = 100
 export var FRICTION = 800
 
-onready var _body = $body
-onready var _head = $head
+var sickIsaac = preload("res://player/sickIsaac.png")
+var isaac = preload("res://player/character_001_isaac.png")
 
-var bath = false
+onready var spriteBody = $SpriteBody
+onready var spriteHead = $SpriteHead
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+var sick = false
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-
-
-
-
-	if Input.is_action_pressed("ui_right"):
-		_body.play("right")
-		_head.play("right")
-		_head.stop()
-		_body.flip_h = false
-		_head.flip_h = false
-
-	elif Input.is_action_pressed("ui_left"):
-		_body.play("left")
-		_head.play("left")		
-		_head.stop()		
-		_body.flip_h = true
-		_head.flip_h = true
-
-	elif Input.is_action_pressed("ui_up"):
-		_body.play("up")
-		_head.play("up")
-		_head.stop()
-
-
-	elif Input.is_action_pressed("ui_down"):
-		_body.play("down")
-		_head.play("down")
-		_head.stop()
 	
-	else:
-		_body.stop()
-		_body.frame = 0
+	
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		launchProjectile(input_vector, 400, "res://Entity/Arrow.tscn")
-	
+		if !sick:
+			spriteBody.set_texture(sickIsaac)
+			spriteHead.set_texture(sickIsaac)
+			sick = true
+		else:
+			spriteBody.set_texture(isaac)
+			spriteHead.set_texture(isaac)
+			sick = false
+		#launchProjectile(input_vector, 400, "res://Entity/Arrow.tscn")
 
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-#	look_at(get_global_mouse_position())
-
-	move(delta, input_vector, ACCELERATION, MAX_SPEED, FRICTION)
 	
-	
+	velocity = move_and_slide(velocity)
 	#for i in get_slide_count():
 	#	var collision = get_slide_collision(i)
 	#	collision.collider.damage(1)
